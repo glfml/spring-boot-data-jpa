9fffefbaca87fb6a8317fce2b43e0c61bcc6d513
Gastón Furini <gfurini@kinexo.com>
2018-12-18 10:28:27 -0300

    Agregar vista y sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Clientes (id, nombre, apellido, email, created_at) VALUES (1, 'Juan', 'Perez', 'juan@perez.com', '2018-12-17');
+
+INSERT INTO Clientes (id, nombre, apellido, email, created_at) VALUES (2, 'Maria', 'García', 'maria@garcia.com', '2018-12-18');
