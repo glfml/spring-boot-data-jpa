1d8dd209cb66b9b5e82baef120846c034e9be9a9
Gastón Furini <gfurini@kinexo.com>
2018-12-18 10:14:24 -0300

    Agregar model y controller

---
+++ src/main/java/com/meli/datajpa/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,23 @@
+package com.meli.datajpa.app.models.dao;
+
+import com.meli.datajpa.app.models.entity.Cliente;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("clienteDaoJPA")
+public class ClienteDaoImpl implements IClienteDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly=true)
+    @Override
+    public List<Cliente> findAll() {
+        return em.createQuery("from Cliente").getResultList();
+    }
+}
---
+++ src/main/java/com/meli/datajpa/app/models/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.meli.datajpa.app.models.dao;
+
+import com.meli.datajpa.app.models.entity.Cliente;
+import java.util.List;
+
+public interface IClienteDao {
+
+    public List<Cliente> findAll();
+
+
+}
