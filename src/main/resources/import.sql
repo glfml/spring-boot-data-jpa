b26d4ec7877e617bd8f799de9409ffbc8bdf7fdf
Gastón Furini <gfurini@kinexo.com>
2018-12-18 15:57:58 -0300

    Usar mysql como motor de base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO Clientes (id, nombre, apellido, email, created_at) VALUES (1, 'Juan', 'Perez', 'juan@perez.com', '2018-12-17');
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES (1, 'Juan', 'Perez', 'juan@perez.com', '2018-12-17');
 
-INSERT INTO Clientes (id, nombre, apellido, email, created_at) VALUES (2, 'Maria', 'García', 'maria@garcia.com', '2018-12-18');
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES (2, 'Maria', 'García', 'maria@garcia.com', '2018-12-18');
